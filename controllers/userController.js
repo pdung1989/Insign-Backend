62c28dc88edd941b9fd495dafcec0fa516719817
Dung Tran <pdung1989@gmail.com>
2021-12-08 22:05:25 +0200

    handling error of user

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,24 +1,38 @@
-'use strict'
-const { getUser, getAllUsers, insertUser, updateUser, deleteUser, getAllPostsOfUser} = require('../models/userModel');
- 
+'use strict';
+const { validationResult } = require('express-validator');
+const {
+  getUser,
+  getAllUsers,
+  insertUser,
+  updateUser,
+  deleteUser,
+  getAllPostsOfUser,
+} = require('../models/userModel');
+const httpError = require('../utils/errors');
+
 // get all users
-const user_list_get = async (req, res) => {
-  const users = await getAllUsers();
-  users.map(user => delete user.password); // delete user's password before sending data
-  console.log('all users', users);
-  res.json(users);
-}
+const user_list_get = async (req, res, next) => {
+  const users = await getAllUsers(next);
+  if (users.length > 0) {
+    users.map((user) => delete user.password); // delete user's password before sending data
+    res.json(users);
+    return;
+  }
+  const err = httpError('Users not found', 404);
+  next(err);
+};
 
 // get user by userId
-const user_get = async (req, res) => {
-  const user = await getUser(req.params.userId);
+const user_get = async (req, res, next) => {
+  const user = await getUser(req.params.userId, next);
   if (user) {
     delete user.password;
     res.json(user);
-  } else {
-    res.json({message: 'user not found'});
+    return;
   }
-}
+  const err = httpError('User not found', 404);
+  next(err);
+};
 
 // add new user
 const user_post = async (req, res) => {
@@ -29,21 +43,26 @@ const user_post = async (req, res) => {
 // delete user
 const user_delete = async (req, res) => {
   const deletedUser = await deleteUser(req.params.userId);
-  res.json({message: 'user deleted'});
-}
+  res.json({ message: 'user deleted' });
+};
 
 // update user
 const user_update = async (req, res) => {
   const updatedUser = await updateUser(req.params.userId, req.body);
 
-  res.json({message: 'user is updated'});
-}
+  res.json({ message: 'user is updated' });
+};
 
 // get posts by userId
-const user_get_posts = async (req, res) =>  {
-  const userPosts = await getAllPostsOfUser(req.params.userId);
+const user_get_posts = async (req, res, next) => {
+  const userPosts = await getAllPostsOfUser(req.params.userId, next);
+  if(userPosts.length === 0) {
+    const err = httpError('Posts of a user not found', 404);
+    next(err);
+    return;
+  }
   res.json(userPosts);
-}
+};
 
 // check token
 const checkToken = (req, res, next) => {
@@ -52,7 +71,7 @@ const checkToken = (req, res, next) => {
   } else {
     res.json({ user: req.user });
   }
- };
+};
 
 module.exports = {
   user_list_get,
@@ -62,4 +81,4 @@ module.exports = {
   user_update,
   user_get_posts,
   checkToken,
-}
+};
--- models/userModel.js
+++ models/userModel.js
@@ -2,20 +2,23 @@
 
 // access database
 const pool = require('../database/db');
+const { httpError } = require('../utils/errors');
 const promisePool = pool.promise();
 
 // get all users
-const getAllUsers = async () => {
+const getAllUsers = async (next) => {
   try {
     const [rows] = await promisePool.execute('SELECT * FROM insign_user');
     return rows;
   } catch (error) {
-    console.log(error.message);
+    console.error('model get all users', e.message);
+    const err = httpError('Sql error', 500);
+    next(err);
   }
 };
 
 // get user by Id
-const getUser = async (userId) => {
+const getUser = async (userId, next) => {
   try {
     const [rows] = await promisePool.execute(
       'SELECT * FROM insign_user where user_id = ?',
@@ -23,7 +26,9 @@ const getUser = async (userId) => {
     );
     return rows[0];
   } catch (error) {
-    console.log(error.message);
+    console.error('model get user by id', e.message);
+    const err = httpError('Sql error', 500);
+    next(err);
   }
 };
 
@@ -82,7 +87,7 @@ const updateUser = async (userId, user) => {
 };
 
 //get all Posts of a user with number of comments and likes
-const getAllPostsOfUser = async (userId) => {
+const getAllPostsOfUser = async (userId, next) => {
   try {
     const [rows] = await promisePool.execute(
       'SELECT post.post_id, post.title, post.image, (SELECT count(*) from likes WHERE likes.post_id = post.post_id) as num_likes, (SELECT count(*) from comment WHERE comment.post_id = post.post_id) as num_comments FROM post WHERE post.author = ?',
@@ -90,7 +95,8 @@ const getAllPostsOfUser = async (userId) => {
     );
     return rows;
   } catch (error) {
-    console.log(error.message);
+    const err = httpError('Sql error', 500);
+    next(err);
   }
 };
 
